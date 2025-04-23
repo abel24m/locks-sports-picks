7f5f6ef679452cd600bbcfefad88c83781a480a4
abel24m <abel24m@utexas.edu>
2025-04-23 14:18:48 -0500

    remove unused hooks

--- src/app/nba-picks/page.tsx
+++ src/app/nba-picks/page.tsx
@@ -7,7 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function createData(
   name: string,
