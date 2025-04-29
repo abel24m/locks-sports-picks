7d1b4b56f3af34070500891025714704a1cb0f32
abel24m <abel24m@utexas.edu>
2025-04-29 10:48:43 -0500

    changed list for fun

--- api/api.py
+++ api/api.py
@@ -1,6 +1,5 @@
 import time
 
-import schedule
 from flask import Flask
 
 app = Flask(__name__)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,11 +61,9 @@ function createData(
 
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Carlos', 1, -2, 0 , 5.3),
+  createData('Allen', 3, 69, 0, 5.0),
+  createData('Joey', 200, 15, 0, 6.0),
 ];
 
 export default function BasicTable() {
@@ -74,11 +72,11 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Goon (100g serving)</TableCell>
+            <TableCell align="right">Bitches</TableCell>
+            <TableCell align="right">Aura&nbsp;(g)</TableCell>
+            <TableCell align="right">Fantasy Championships&nbsp;(g)</TableCell>
+            <TableCell align="right">40 yard time&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
