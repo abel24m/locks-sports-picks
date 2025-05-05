3ebb98ec5ea34a4937973dcfc9950d6813ca4e2b
abel24m <abel24m@utexas.edu>
2025-05-05 17:06:00 -0500

    error issue

--- api/api.py
+++ api/api.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 # schedule.every().day.at("10:30").do(web_scrape)
 
+import os
+
+project_path = os.getcwd()
+
 @app.route('/mlb_data')
 def get_current_time():
-    with open ('./web_scrape/data_storage/covers_mlb_data_05-05-2025.json') as file:
+    with open (f'{project_path}/web_scrape/data_storage/covers_mlb_data_05-05-2025.json') as file:
         data = json.load(file)
     return data
 
